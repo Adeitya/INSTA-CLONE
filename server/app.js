90ed951a7e480781e45453abaceb27dc2465499e
adeitya <adityabagri7628@gmail.com>
2022-04-19 15:52:04 +0530

    sign in

--- server/app.js
+++ server/app.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const app=express()
-const mongoose = require('mongoose')
-const PORT=5000
-const {MONGOURI}= require('./keys')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = 5000;
+const { MONGOURI } = require("./keys");
 
-require('./models/user')
+require("./models/user");
 
-mongoose.connect(MONGOURI)
-mongoose.connection.on('connected',()=>{
-    console.log("connected to mongoose")
-})
-mongoose.connection.on('error',(err)=>{
-    console.log("err connecting",err)
-})
+app.use(express.json());
+app.use(require("./routes/auth"));
 
+mongoose.connect(MONGOURI);
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongoose");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("err connecting", err);
+});
 
-app.listen(PORT,()=>{
-    console.log("server is running on",PORT)
-})
+app.listen(PORT, () => {
+  console.log("server is running on", PORT);
+});
