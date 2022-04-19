e66433a8e7ed27bd0564e5144048f7d7d3150ec4
adeitya <adityabagri7628@gmail.com>
2022-04-19 15:50:03 +0530

    authorization completed

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
-module.exports={
-    MONGOURI:"mongodb+srv://Adeitya:qJBCykuw97NBn9tX@cluster0.qsybb.mongodb.net/test?retryWrites=true&w=majority"
-}
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://Adeitya:qJBCykuw97NBn9tX@cluster0.qsybb.mongodb.net/test?retryWrites=true&w=majority",
+};
