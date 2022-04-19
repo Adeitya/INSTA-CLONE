82066bcc8649eb7d63727b828efc055870087130
adeitya <adityabagri7628@gmail.com>
2022-04-19 10:37:53 +0530

    sign in

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app=express()
+const mongoose = require('mongoose')
+const PORT=5000
+const {MONGOURI}= require('./keys')
+
+require('./models/user')
+
+mongoose.connect(MONGOURI)
+mongoose.connection.on('connected',()=>{
+    console.log("connected to mongoose")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:"mongodb+srv://Adeitya:qJBCykuw97NBn9tX@cluster0.qsybb.mongodb.net/test?retryWrites=true&w=majority"
+}
